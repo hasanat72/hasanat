fba051038b960cfd449de3c25e944cf767713ba1
Ali Hasanat <33457974+hasanat72@users.noreply.github.com>
2017-12-29 12:23:21 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hasanat</title>
+</head>
+<audio autoplay loop>
+	<!-- <source src="Empire Syndicate - Last Breath.mp3" type="audio/mpeg" autoplay>-->
+</audio>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<body class="main">
+	
+		<div>
+		<div class="container">
+			<img src="image/img1.jpg" class="image">
+			
+		</div>
+		<div class="side_div">
+			<h3 class="text">Ali Hasanat</h3>
+
+		</div>
+		<br>
+		<div class="info">
+			<div class="location">
+				<h5>Bangladesh</h5>
+				
+			</div>
+			<div class="birth">
+				<h5>December 8,1991</h5>
+			</div>
+			<div class="languages">
+				<p>Programming Languages:</p>
+				
+			</div>
+			<div class="language">
+					<p> C <br> JAVA <br>PHP <br>HTML <br>CSS <br>MySQL <br>Python <br></p> 
+				</div>
+			<div class="social">
+				<a href="https://www.linkedin.com/in/ali-hasanat-b6aa4478/" class="fa fa-linkedin" target="_blank"></a>
+				<a href="https://github.com/hasanat72" class="fa fa-github" target="_blank"></a>
+				<a href="https://www.facebook.com/ali.hasanat72" class="fa fa-facebook" target="_blank"></a>
+				<a href="landing.html" class="fa fa-globe" target="_blank"></a>
+				<a href="hobby.html" class="fa fa-camera" target="_blank"></a>
+			</div>
+			<div id="education">
+				<h4 style="color: #34495e">Education:</h4>	
+				<p style="color:  #34495e; margin-top: -20px;margin-left: 40px">Computer Science and Engineering<br>NSU</p>
+				
+			</div>
+			<div></div>
+			<div class="project">
+				<p>Projects:</p>
+				
+			</div>
+
+				<div class="projects">
+					<p>1.Photography Club management<br>System</p>
+					<p>2.GPS Based Car Monitoring<br>System</p>
+					<p>3.Car Pooling<br>System</p>
+
+				</div>
+				</div>
+			</div>
+			<br>
+			<div id="contact">
+					<form action="contact.html">
+						<p><input class="button" type="submit" value="Contact Me"></p>
+					</form>
+			</div>
+			
+</body>
+</body>
+
+</html>
